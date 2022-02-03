ac6c4e5d8eb843b5d78cb56c843e9be19f019891
Abhishek Mishra <amishr1@stevens.edu>
2022-02-03 07:28:31 -0500

    lab2

--- Lab 2/app.js
+++ Lab 2/app.js
@@ -1,31 +1,13 @@
 const express = require("express");
 const app = express();
-app.use(express.json);
 
-const bluebird = require("bluebird");
-const redis = require("redis");
-const client = redis.createClient();
+const users = require("./routes");
+app.use("/api/people", users);
 
-bluebird.promisifyAll(redis.RedisClient.prototype);
-bluebird.promisifyAll(redis.Multi.prototype);
-
-app.get("/api/people/:id", async (req, res) => {
-    try {
-        const user = await data.getById(req.params.id);
-        res.json(user);
-        let users = await client.existsAsync("users");
-        if (!users) {
-            console.log("users");
-        }
-    } catch (e) {
-        console.log(e);
-        res.status(404).send(e.message);
-    }
-});
 app.use("*", async (req, res) => {
     res.status(404).json({ error: "404! Not Found" });
 });
 
-app.listen(3001, () => {
+app.listen(3000, () => {
     console.log("Server started at port 3000");
 });
--- Lab 2/routes.js
+++ Lab 2/routes.js
@@ -5,17 +5,33 @@ const data = require("./data");
 const bluebird = require("bluebird");
 const redis = require("redis");
 const client = redis.createClient();
-console.log(client);
+
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
+router.get("/history", async (req, res) => {
+    try {
+        let history = await client.lrangeAsync("list", 0, 19);
+        res.json(history.map(JSON.parse));
+    } catch (e) {
+        console.log(e);
+        res.status(404).send(e.message);
+    }
+});
+
 router.get("/:id", async (req, res) => {
     try {
-        const user = await data.getById(req.params.id);
-        res.json(user);
-        let users = await client.existsAsync("users");
-        if (!users) {
-            console.log("users");
+        if ((await client.hexistsAsync("users", req.params.id)) === 1) {
+            const user = await client.hmgetAsync("users", req.params.id);
+            res.json(JSON.parse(user));
+            await client.lpushAsync("list", user);
+            return;
+        } else {
+            const user = await data.getById(req.params.id);
+            res.json(user);
+            await client.hmsetAsync("users", req.params.id, JSON.stringify(user));
+            await client.lpushAsync("list", JSON.stringify(user));
+            return;
         }
     } catch (e) {
         console.log(e);
